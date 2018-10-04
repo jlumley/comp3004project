788093ab63648f0927668bfacef45ca62850e770
A A <Steve@As-MacBook-Air.local>
2018-10-04 16:15:41 -0400

    tile test class added with an exampl junit test case. Next commits will be in individual branch but I pushed to master since this is the first push for us

---
+++ src/test/java/core/TestTile.java
@@ -0,0 +1,13 @@
+package core;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTile
+{
+		@Test
+		public void exampleTest()
+		{
+			assertEquals(1, 2);
+		}
+}
