b0f80567dd923fca725a7af07b1ecd79ab0afe6a
Steven Fang <stevenfang.cu@gmail.com>
2018-10-06 16:25:50 -0400

    test to show the main deck from the main program.

---
+++ src/test/java/core/mainTest.java
@@ -0,0 +1,10 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class mainTest extends TestCase{
+	public void testMain() {
+		TileRummyMain result = new TileRummyMain();
+		result.showDeck();
+	}
+}
