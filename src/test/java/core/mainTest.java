163a2582782ffbd65c29bcba96e489db006dcfb5
Steven Fang <stevenfang.cu@gmail.com>
2018-10-06 17:06:36 -0400

    touched up test case

--- src/test/java/core/mainTest.java
+++ src/test/java/core/mainTest.java
@@ -2,11 +2,13 @@ package core;
 
 import junit.framework.TestCase;
 
+
 public class mainTest extends TestCase{
-	public void testMain() {
+	public void testMain1() {
 		TileRummyMain result = new TileRummyMain();
 		result.initialize();
 		result.showDeck();
 		result.showHands();
 	}
+	
 }
