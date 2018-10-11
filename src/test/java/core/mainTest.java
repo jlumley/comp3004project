bfdfced51190373e6aecd47a5f5311d2ee9da43c
Michael Kameoka <michaelkameoka@cmail.carleton.ca>
2018-10-11 14:26:21 -0400

    Test Main: added Test for buildDeck() and Test for dealing initial Hands

--- src/test/java/core/mainTest.java
+++ src/test/java/core/mainTest.java
@@ -10,5 +10,23 @@ public class mainTest extends TestCase{
 		result.showDeck();
 		result.showHands();
 	}
+	public void testBuildDeck() {
+		TileRummyMain result = new TileRummyMain();
+		assertNotNull(result.buildDeck(result.suites, result.values));
+		assertEquals(104, result.buildDeck(result.suites, result.values).size());
+		
+	}
+	public void testdealHands() {
+		TileRummyMain result = new TileRummyMain();
+		result.initialize();
+		assertNotNull(result.player0.getHand());
+		assertNotNull(result.player1.getHand());
+		assertNotNull(result.player2.getHand());
+		assertNotNull(result.player3.getHand());
+		assertEquals(14, result.player0.getHand().size());
+		assertEquals(14, result.player1.getHand().size());
+		assertEquals(14, result.player2.getHand().size());
+		assertEquals(14, result.player3.getHand().size());
+	}
 	
 }
