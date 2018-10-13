e7528c2ba8ee52d7fa90eaea0ff7e87e1fdff767
Steven Fang <stevenfang.cu@gmail.com>
2018-10-13 19:56:13 -0400

    test cases for showField, checkSizeofHand -> rename into sizeOfMend and checkGameStatus

--- src/test/java/core/mainTest.java
+++ src/test/java/core/mainTest.java
@@ -1,14 +1,28 @@
 package core;
 
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 
 public class mainTest extends TestCase{
 	public void testMain1() {
 		TileRummyMain result = new TileRummyMain();
+		
+		ArrayList<Tile> temp = new ArrayList<Tile>();
+		Tile t1 = new Tile("B", 10);
+		temp.add(t1);
+		Tile t2 = new Tile("O", 10);
+		temp.add(t2);
+		Tile t3 = new Tile("B", 10);
+		temp.add(t3);
+		
 		result.initialize();
 		result.showDeck();
 		result.showHands();
+		result.showField();
+		assertEquals(true,result.checkSizeofHand(temp));
+		assertEquals(false,result.checkGameStatus()); //checks if any user has an empty hand
 	}
 	public void testBuildDeck() {
 		TileRummyMain result = new TileRummyMain();
@@ -23,10 +37,10 @@ public class mainTest extends TestCase{
 		assertNotNull(result.player1.getHand());
 		assertNotNull(result.player2.getHand());
 		assertNotNull(result.player3.getHand());
-		assertEquals(14, result.player0.getHand().size());
-		assertEquals(14, result.player1.getHand().size());
-		assertEquals(14, result.player2.getHand().size());
-		assertEquals(14, result.player3.getHand().size());
+		assertEquals(13, result.player0.getHand().size());
+		assertEquals(13, result.player1.getHand().size());
+		assertEquals(13, result.player2.getHand().size());
+		assertEquals(0, result.player3.getHand().size());
 	}
 	
 }
