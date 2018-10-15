4f7284081bed0eb2566de05c0d07ea73203a75be
SARCASMOOO <ssstevenstapleton@gmail.com>
2018-10-15 13:50:07 -0400

    Test cases for GUI Some test cases will be written for gui then imlemented in GUI class. The rest of the test cases will be written after but will still be coded before they are imlemented in GUI.

--- src/test/java/core/TestGUI.java
+++ src/test/java/core/TestGUI.java
@@ -7,34 +7,32 @@ import junit.framework.TestCase;
 public class TestGUI extends TestCase
 {
 	@Test
-	public void testMoveCordinates()
+	public void testDealHand()
 	{
-		assertEquals(1,2);
+		/* This functions will give each player and AI there hand */
+		assertEquals(1, 2);
 	}
 	@Test
-	public void testMeld()
+	public void testDragTile()
 	{
-		assertEquals(1,2);
+		/* Check if user is allowed to drag tile then drag the tile */
+		/* if tile is outof bounds when released put it back */
+		/* auto attach to melds */
+		assertEquals(1, 2);
 	}
 	@Test
-	public void testDrawCard()
+	public void testDisplayMessage()
 	{
-		assertEquals(1,2);
+		/* This will be used when AI said there action and serves as a message dispatch for anything else */
+		assertEquals(1, 2);
 	}
 	@Test
-	public void testDragCard()
+	public void testDisplayWinner()
 	{
-		assertEquals(1,2);
-	}
-	@Test
-	public void testWinGame()
-	{
-		assertEquals(1,2);
-	}
-	@Test
-	public void testLostGame()
-	{
-		assertEquals(1,2);
+		/* Show winner this will happen at end of game */
+		assertEquals(1, 2);
 	}
 	
+	//TODO
+	//Make test and implementations to deal with auto attaching to melds
 }
