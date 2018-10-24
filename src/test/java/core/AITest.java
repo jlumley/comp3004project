432c3e0538bc86849f23e19aeec7ae75ef46ac37
Michael Kameoka <michaelkameoka@cmail.carleton.ca>
2018-10-24 18:14:04 -0400

    Test: Test for a AI Player1 playTurn()

--- src/test/java/core/AITest.java
+++ src/test/java/core/AITest.java
@@ -3,5 +3,19 @@ package core;
 import junit.framework.TestCase;
 
 public class AITest extends TestCase{
-	
+	public void testAddToField() { //tests if adding given tiles to the board could have a valid board
+		
+	}
+	public void testValidField() { //checks if the board contains all valid melds without extra tiles
+		
+	}
+	public void testAIPlayer1() {
+		TileRummyMain result = new TileRummyMain();
+		result.initialize();
+		result.player1.showHand();
+		result.showField();
+		result.player1.playTurn();
+		result.player1.showHand();
+		result.showField();
+	}
 }
