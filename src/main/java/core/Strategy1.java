d784c0444696f16d8082e90d78795e00af62d411
Michael Kameoka <michaelkameoka@cmail.carleton.ca>
2018-10-24 18:15:17 -0400

    Strategy1: NOT fully functional yet, simple version of playing using existing methods

--- src/main/java/core/Strategy1.java
+++ src/main/java/core/Strategy1.java
@@ -1,9 +1,16 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Strategy1 implements AIStrategy{
 	public void playTurn() {
 		//Check for first turn 30 points
-		
+		ArrayList<ArrayList<Tile>> p1Melds = TileRummyMain.player1.inHandMelds();
+		if(TileRummyMain.player1.checkPlays(p1Melds)) {
+			for(int i = 0; i < p1Melds.size(); i++) {
+				TileRummyMain.addMend(p1Melds.get(i));
+			}
+		}
 		//Play all the tiles it can
 		
 	}
