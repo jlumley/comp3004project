3aed152b422af8cd58f0e329bb6bc621b4b22aa7
Michael Kameoka <michaelkameoka@cmail.carleton.ca>
2018-12-02 21:56:23 -0500

    Refactored Strategy 1: fixed drawing after playing cards problem

--- src/main/java/core/Strategy1.java
+++ src/main/java/core/Strategy1.java
@@ -4,35 +4,79 @@ import java.util.ArrayList;
 
 public class Strategy1 implements AIStrategy{
 	public void playTurn() {
-		ArrayList<ArrayList<Tile>> p1Melds = TileRummyMain.player1.playAllTiles();
-			//if not >30 put all tiles back in Player's Hand & draw Card
 		System.out.println("Player 1 Turn");
 		System.out.println("Player 1 Hand: " + TileRummyMain.player1.getHand());
+		ArrayList<ArrayList<Tile>> p1Melds = TileRummyMain.player1.playAllTiles();
 		System.out.println("Possible Melds in Hand: " + p1Melds);
-		if(TileRummyMain.player1.checkPlays(p1Melds)) { //checks for >=30 size
-			for(int i = 0; i < p1Melds.size(); i++) { //Play all the tiles it can
-				TileRummyMain.addMend(p1Melds.get(i)); //add all melds to board
+		if(TileRummyMain.player1.firstTurn == false) { //first turn has not passed yet
+			if(TileRummyMain.player1.checkPlays(p1Melds)) { //checks for >=30 size
+				for(int i = 0; i < p1Melds.size(); i++) { //Play all the tiles it can
+					TileRummyMain.addMend(p1Melds.get(i)); //add all melds to board
+				}
+				TileRummyMain.player1.firstTurn = true;
 			}
-			TileRummyMain.player1.firstTurn = true;
+			else { //returns any unused Tiles back to hand, and draws a Tile
+				returnCardsAndDrawCard(p1Melds);
+			}
 		}
-		else { //returns any unused Tiles back to hand, and draws a Tile
-			//return unPlayed Melds back into Hand
-			for(int i = 0; i < p1Melds.size(); i++) { //double loops in and adds all Tiles back
-				for(int j = 0; j < p1Melds.get(i).size(); j++) {
-					TileRummyMain.player1.addTile(p1Melds.get(i).get(j));
+		else {
+			ArrayList<ArrayList<Tile>> p1NewBoard = TileRummyMain.player1.playAllTiles(TileRummyMain.field);
+			//Check if can win this turn, if so Win
+			if(TileRummyMain.player1.countTiles() == 0) {
+				TileRummyMain.field = p1NewBoard; //replace old board with this new one
+				for(int i = 0; i < p1Melds.size(); i++) { 
+					TileRummyMain.addMend(p1Melds.get(i)); 
+				}
+				return;
+			}
+			//play all tiles including board reuse
+			if(TileRummyMain.field.size() != p1NewBoard.size()) { //check num of melds
+				TileRummyMain.field = p1NewBoard; //replace old board with this new one
+				for(int i = 0; i < p1Melds.size(); i++) { 
+					TileRummyMain.addMend(p1Melds.get(i)); 
+				}
+				return;
+			}
+			else { //if equal num melds
+				for(int i = 0; i < TileRummyMain.field.size(); i++) { //check size of each meld
+					if(TileRummyMain.field.get(i).size() != p1NewBoard.get(i).size()) {
+						TileRummyMain.field = p1NewBoard; //replace old board with this new one
+						for(int k = 0; k < p1Melds.size(); k++) { 
+							TileRummyMain.addMend(p1Melds.get(k)); 
+						}
+						return;
+					}
 				}
 			}
-			//draw a Tile
-			if(!TileRummyMain.initDeck.isEmpty()){ //if not empty NORMAL CASE
-				System.out.println("Player 1 draws: " + TileRummyMain.initDeck.get(0));
-				TileRummyMain.player1.drawTile(TileRummyMain.initDeck);
+			//no board re-use available but try just regular meld playing
+			if(!p1Melds.isEmpty()) {//not empty
+				for(int i = 0; i < p1Melds.size(); i++) { //Play all the tiles it can
+					TileRummyMain.addMend(p1Melds.get(i)); //add all melds to board
+				}
 			}
-			else { //just when doing partial tests
-				TileRummyMain.initDeck = TileRummyMain.buildDeck(TileRummyMain.suites, TileRummyMain.values);
-				System.out.println("Player 1 draws: " + TileRummyMain.initDeck.get(0));
-				TileRummyMain.player1.drawTile(TileRummyMain.initDeck);
-			}
+			//otherwise
+			System.out.println("p1 could play but has no tile to play");
+			returnCardsAndDrawCard(p1Melds);
 		}
 		
+	}
+	//returns any unused Tiles back to hand, and draws a Tile
+	public void returnCardsAndDrawCard(ArrayList<ArrayList<Tile>> p1Melds) {
+		//return unPlayed Melds back into Hand
+		for(int i = 0; i < p1Melds.size(); i++) { //double loops in and adds all Tiles back
+			for(int j = 0; j < p1Melds.get(i).size(); j++) {
+				TileRummyMain.player1.addTile(p1Melds.get(i).get(j));
+			}
+		}
+		//draw a Tile
+		if(!TileRummyMain.initDeck.isEmpty()){ //if not empty NORMAL CASE
+			System.out.println("Player 1 draws: " + TileRummyMain.initDeck.get(0));
+			TileRummyMain.player1.drawTile(TileRummyMain.initDeck);
+		}
+		else { //just when doing partial tests
+			TileRummyMain.initDeck = TileRummyMain.buildDeck(TileRummyMain.suites, TileRummyMain.values);
+			System.out.println("Player 1 draws: " + TileRummyMain.initDeck.get(0));
+			TileRummyMain.player1.drawTile(TileRummyMain.initDeck);
+		}
 	}
 }
